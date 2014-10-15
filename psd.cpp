3a85efa8fb02637d81179f163adb40d60a8ee59f
Jaeseung Ha <ipknhama@gmail.com>
2014-10-16 00:16:52 +0900

    Layer information reading except image data, Big Endian integer type added

--- psd.cpp
+++ psd.cpp
@@ -1,4 +1,5 @@
 #include "psd.h"
+#include <cassert>
 
 #define PSD_DEBUG
 
@@ -19,7 +20,7 @@ namespace psd
             padded_size<2>(buffer.size());
     }
 
-    bool ImageResourceBlock::read(std::istream&& stream)
+    bool ImageResourceBlock::read(std::istream& stream)
     {
 #ifdef PSD_DEBUG
         auto start_pos = stream.tellg();
@@ -34,7 +35,6 @@ namespace psd
         }
 
         stream.read((char*)&image_resource_id, 2);
-        BEtoLE(image_resource_id);
 
         uint8_t length;
         stream.read((char*)&length, 1);
@@ -43,9 +43,8 @@ namespace psd
         if (length % 2 == 0)
             stream.seekg(1, std::ios::cur);
 
-        uint32_t buffer_length{};
+        be<uint32_t> buffer_length;
         stream.read((char*)&buffer_length, 4);
-        BEtoLE(buffer_length);
         buffer.resize(buffer_length);
         stream.read(&buffer[0], buffer_length);
         if (buffer_length % 2 == 1)
@@ -60,15 +59,13 @@ namespace psd
         return true;
     }
 
-    bool ImageResourceBlock::write(std::ostream&& stream)
+    bool ImageResourceBlock::write(std::ostream& stream)
     {
 #ifdef PSD_DEBUG
         auto start_pos = stream.tellp();
 #endif
         stream.write("8BIM", 4);
-        BEtoLE(image_resource_id);
         stream.write((char*)&image_resource_id, 2);
-        BEtoLE(image_resource_id);
 
         char padding = 0;
 
@@ -78,8 +75,7 @@ namespace psd
         if (length % 2 == 0)
             stream.write(&padding, 1);
 
-        uint32_t buffer_length = buffer.size();
-        BEtoLE(buffer_length);
+        be<uint32_t> buffer_length = buffer.size();
         stream.write((char*)&buffer_length, 4);
         stream.write(buffer.data(), buffer.size());
         if (buffer.size() % 2 == 1)
@@ -97,29 +93,25 @@ namespace psd
     {
     }
 
-    psd::psd(std::istream&& stream)
+    bool psd::load(std::istream& stream)
     {
-        load(std::move(stream));
-    }
-
-    bool psd::load(std::istream&& stream)
-    {
-        if (!read_header(std::move(stream)))
+        if (!read_header(stream))
             return false;
-        if (!read_color_mode(std::move(stream)))
+        if (!read_color_mode(stream))
             return false;
-        if (!read_image_resources(std::move(stream)))
+        if (!read_image_resources(stream))
+            return false;
+        if (!read_layers_and_masks(stream))
             return false;
 
         valid_ = true;
         return true;
     }
 
-    bool psd::read_header(std::istream&& f)
+    bool psd::read_header(std::istream& f)
     {
         f.seekg(0);
         f.read((char*)&header, sizeof(header));
-        header.convert();
 
         if (header.signature != *(uint32_t*)"8BPS")
             return false;
@@ -141,7 +133,7 @@ namespace psd
         return true;
     }
 
-    bool psd::read_color_mode(std::istream&& f)
+    bool psd::read_color_mode(std::istream& f)
     {
         uint32_t count;
         f.read((char*)&count, sizeof(count));
@@ -153,11 +145,10 @@ namespace psd
         return true;
     }
 
-    bool psd::read_image_resources(std::istream&& f)
+    bool psd::read_image_resources(std::istream& f)
     {
-        uint32_t length;
+        be<uint32_t> length;
         f.read((char*)&length, 4);
-        BEtoLE(length);
 #ifdef PSD_DEBUG
         std::cout << "Image Resource Block length: " << length << std::endl;
 #endif
@@ -168,38 +159,248 @@ namespace psd
         while(f.tellg() - start_pos < length)
         {
             ImageResourceBlock b;
-            if (!b.read(std::move(f)))
+            if (!b.read(f))
                 return false;
             image_resources.push_back(std::move(b));
         }
         return true;
     }
 
-    bool psd::save(std::ostream&& f)
+    bool Layer::LayerBlendingRanges::read(std::istream& f)
     {
-        if (!write_header(std::move(f)))
+        be<uint32_t> size;
+        f.read((char*)&size, 4);
+#ifdef PSD_DEBUG
+        std::cout << "Reading blending ranges (size: " << size << ")" << std::endl;
+#endif
+        data.resize(size);
+        f.read(&data[0], size);
+        return true;
+    }
+    
+    bool Layer::LayerMask::read(std::istream& f)
+    {
+        f.read((char*)&size, 4);
+#ifdef PSD_DEBUG
+        std::cout << "Reading mask (size: " << size << ")" << std::endl;
+#endif
+        if (size)
+        {
+            f.read((char*)&top, 4*4+2);
+            uint32_t remaining = size - (4*4+2);
+            additional_data.resize(remaining);
+            f.read(&additional_data[0], remaining);
+        }
+        return true;
+    }
+
+    bool Layer::read(std::istream& f)
+    {
+        f.read((char*)&top, 4*4+2);
+#ifdef PSD_DEBUG
+        std::cout << '\t' << top << ' ' << left <<' ' <<bottom << ' ' << right << std::endl;
+        std::cout << "Number of channels: " << num_channels << std::endl;
+#endif
+        for(uint16_t i = 0; i < num_channels; i ++)
+        {
+            char buffer[6];
+            f.read(buffer, 6);
+            channel_infos.emplace_back(*(be<uint16_t>*)buffer, *(be<uint32_t>*)(buffer+2));
+        }
+        f.read((char*)&blend_signature, 4*3+4);
+#ifdef PSD_DEBUG
+        std::cout << "Blend Signature: " << std::string((char*)&blend_signature, (char*)&blend_signature+4) << std::endl;
+#endif
+        if ((*(uint32_t*)"8BIM") != blend_signature)
             return false;
-        if (!write_color_mode(std::move(f)))
+
+        auto extra_start_pos = f.tellg();
+
+        if (!mask.read(f))
+            return false;
+        if (!blending_ranges.read(f))
+            return false;
+
+        uint8_t name_size;
+        f.read((char*)&name_size, 1);
+        name.resize(name_size);
+        f.read(&name[0], name_size);
+        switch(name_size%4)
+        {
+            case 0:
+                f.seekg(3, std::ios::cur);
+                break;
+            case 1:
+                f.seekg(2, std::ios::cur);
+                break;
+            case 2:
+                f.seekg(1, std::ios::cur);
+                break;
+            case 3:
+                break;
+        }
+        auto remaining = extra_data_length - (f.tellg()-extra_start_pos);
+        additional_extra_data.resize(remaining);
+        f.read(&additional_extra_data[0], remaining);
+        for(char c:name)
+            wname += (wchar_t)c;
+        char* p = &additional_extra_data[0];
+        utf8name = name;
+        while(p < &additional_extra_data[0] + additional_extra_data.size())
+        {
+            if (*(uint32_t*)p != *(uint32_t*)"8BIM" &&
+                *(uint32_t*)p != *(uint32_t*)"8B64" )
+            {
+#ifdef PSD_DEBUG
+                std::cout << "Extra data signature error at: " << f.tellg() << ' ' << std::string(p, p+4) <<std::endl;
+#endif
+
+                return false;
+            }
+            p += 4;
+            std::string key(p, p+4);
+            p += 4;
+            be<uint32_t> length(*(be<uint32_t>*)p);
+            p += 4;
+#ifdef PSD_DEBUG
+            std::cout << '\t' << key;
+#endif
+            if (key == "luni")
+            {
+                be<uint32_t> uni_length = *(be<uint32_t>*)p;
+                wname.clear();
+                for(uint32_t i = 0; i < uni_length; i ++)
+                {
+                    wname += (wchar_t)(uint16_t)*(be<uint16_t>*)(p+4+i*2);
+                }
+                utf8name.clear();
+                for(auto wc : wname)
+                {
+                    if (wc < 0x80)
+                        utf8name += (char)wc;
+                    else if (wc < 0x800) //110xxxxx 10xxxxxx
+                    {
+                        utf8name += (char)(0xC0 + ((wc>>6)&0x1F));
+                        utf8name += (char)(0x80 + (wc & 0x3F));
+                    }
+                    else  // 1110xxxx 10xxxxxx 10xxxxxx 6+6+4
+                    {
+                        utf8name += (char)(0xE0 + ((wc>>12)&0x0F));
+                        utf8name += (char)(0x80 + ((wc>>6) & 0x3F));
+                        utf8name += (char)(0x80 + (wc & 0x3F));
+                    }
+                }
+            }
+            else if (key == "TySh")
+            {
+                has_text = true;
+            }
+            p += length;
+        }
+
+#ifdef PSD_DEBUG
+        std::cout << std::endl;
+        std::cout << "Layer " << utf8name << std::endl;
+#endif
+
+        return true;
+    }
+
+    bool Layer::write(std::ostream& f)
+    {
+        // TODO
+        assert(false);
+        return true;
+    }
+
+    bool LayerInfo::read(std::istream& f)
+    {
+        be<uint32_t> length;
+        f.read((char*)&length, 4);
+
+        f.read((char*)&num_layers, 2);
+        
+        if (num_layers < 0)
+        {
+            num_layers = -num_layers;
+            has_merged_alpha_channel = true;
+        }
+
+#ifdef PSD_DEBUG
+        std::cout  << "Number of layers: " << num_layers << std::endl;
+#endif
+
+        for(uint32_t i = 0; i < num_layers; i ++)
+        {
+#ifdef PSD_DEBUG
+            std::cout << "Layer " << i << ":" << std::endl;
+#endif
+            Layer l;
+            if (!l.read(f))
+                return false;
+            layers.push_back(std::move(l));
+        }
+
+        return true;
+    }
+
+    bool LayerInfo::write(std::ostream& f)
+    {
+        // TODO
+        assert(false);
+        return true;
+    }
+
+    bool psd::read_layers_and_masks(std::istream& f)
+    {
+        auto start_pos = f.tellg();
+
+        be<uint32_t> length;
+        f.read((char*)&length, 4);
+        
+        if (length == 0)
+            return true;
+
+        if (!layer_info.read(f))
+            return false;
+
+        //if (!read_global_mask_info(f))
+            //return false;
+
+        if (f.tellg()-start_pos < length)
+        {
+            auto remaining = length - (f.tellg()-start_pos);
+            std::vector<char> additional_layer_data;
+            additional_layer_data.resize(remaining);
+            f.read(&additional_layer_data[0], remaining);
+        }
+
+        return true;
+    }
+
+    bool psd::save(std::ostream& f)
+    {
+        if (!write_header(f))
+            return false;
+        if (!write_color_mode(f))
             return false;
         return true;
     }
 
-    bool psd::write_header(std::ostream&& f)
+    bool psd::write_header(std::ostream& f)
     {
-        header.convert();
         f.write((char*)&header, sizeof(header));
-        header.convert();
         return true;
     }
 
-    bool psd::write_color_mode(std::ostream&& f)
+    bool psd::write_color_mode(std::ostream& f)
     {
         uint32_t t{};
         f.write((char*)&t, 4);
         return true;
     }
 
-    bool psd::write_image_resources(std::ostream&& f)
+    bool psd::write_image_resources(std::ostream& f)
     {
         uint32_t length{};
         for(auto& r:image_resources)
@@ -209,7 +410,7 @@ namespace psd
         f.write((char*)&length, 4);
         for(auto& r:image_resources)
         {
-            if (!r.write(std::move(f)))
+            if (!r.write(f))
                 return false;
         }
         return true;
--- psd.h
+++ psd.h
@@ -27,6 +27,57 @@ namespace psd
         uint8_t* p = reinterpret_cast<uint8_t*>(&x);
         t = p[0]; p[0] = p[1]; p[1] = t;
     }
+    template <> inline void BEtoLE<int32_t>(int32_t& x)
+    {
+        BEtoLE(*(uint32_t*)&x);
+    }
+    template <> inline void BEtoLE<int16_t>(int16_t& x)
+    {
+        BEtoLE(*(uint16_t*)&x);
+    }
+
+
+    template <typename T>
+    struct be
+    {
+        be()
+            : x(0)
+        {
+        }
+
+        be(T x)
+            : x(x)
+        {
+            BEtoLE(x);
+        }
+
+        be(const be& y)
+            : x(y.x)
+        {
+        }
+
+        operator T()
+        {
+            T y = x;
+            BEtoLE(y);
+            return y;
+        }
+
+        be& operator = (be y)
+        {
+            x = y;
+            return *this;
+        }
+
+        be& operator = (T y)
+        {
+            x = y;
+            BEtoLE(y);
+            return *this;
+        }
+
+        T x;
+    };
 
     enum class ColorMode : uint16_t
     {
@@ -44,63 +95,153 @@ namespace psd
     struct Header
     {
         uint32_t signature;
-        uint16_t version;
+        be<uint16_t> version;
         uint16_t dummy1{};
         uint32_t dummy2{};
-        uint16_t num_channels;
-        uint32_t height;
-        uint32_t width;
-        uint16_t bit_depth;
-        uint16_t color_mode;
-
-        void convert()
-        {
-            BEtoLE(version);
-            BEtoLE(num_channels);
-            BEtoLE(height);
-            BEtoLE(width);
-            BEtoLE(bit_depth);
-            BEtoLE(color_mode);
-        }
+        be<uint16_t> num_channels;
+        be<uint32_t> height;
+        be<uint32_t> width;
+        be<uint16_t> bit_depth;
+        be<uint16_t> color_mode;
     };
+
     struct ImageResourceBlock
     {
         uint32_t signature;
-        uint16_t image_resource_id;
+        be<uint16_t> image_resource_id;
         std::string name; // encoded as pascal string; 1 byte length header
 
         std::vector<char> buffer;
 
         uint32_t size() const;
-        bool read(std::istream&& stream);
-        bool write(std::ostream&& stream);
+        bool read(std::istream& stream);
+        bool write(std::ostream& stream);
     };
+
+    struct Layer
+    {
+        be<uint32_t> top, left, bottom, right;
+        be<uint16_t> num_channels;
+        std::vector<std::pair<be<uint16_t>, be<uint32_t>>> channel_infos; // ID, length
+        uint32_t blend_signature;
+        be<uint32_t> blend_key;
+        uint8_t opacity; // 0 for transparent
+        uint8_t clipping; // 0 base, 1 non-base
+        uint8_t bit_flags; 
+        uint8_t dummy1;
+        be<uint32_t> extra_data_length;
+        std::vector<char> additional_extra_data;
+
+        struct LayerMask
+        {
+            be<uint32_t> size;
+            be<uint32_t> top, left, bottom, right;
+            uint8_t default_color;
+            uint8_t flags;
+            std::vector<char> additional_data;
+
+            bool read(std::istream& f);
+            bool write(std::ostream& f)
+            {
+                f.write((char*)&size, 4);
+                if (size)
+                {
+                    f.write((char*)&top, 4*4+2);
+                    uint32_t remaining = size - (4*4+2);
+                    additional_data.resize(remaining);
+                    f.write(&additional_data[0], remaining);
+                }
+                return true;
+            }
+
+        } mask;
+
+        struct LayerBlendingRanges
+        {
+            std::vector<char> data;
+            bool read(std::istream& f);
+
+            bool write(std::ostream& f)
+            {
+                be<uint32_t> size = data.size();
+                f.write((char*)&size, 4);
+                f.write(&data[0], size);
+                return true;
+            }
+        } blending_ranges;
+        std::string name;
+        std::wstring wname;
+        std::string utf8name;
+        bool has_text{};
+
+        bool read(std::istream& f);
+        bool write(std::ostream& f);
+    };
+
+    struct ImageData
+    {
+    };
+
+    struct LayerInfo
+    {
+        be<int16_t> num_layers{};
+        bool has_merged_alpha_channel{};
+        std::vector<Layer> layers;
+        std::vector<ImageData> channel_image_datas;
+
+        bool read(std::istream& stream);
+        bool write(std::ostream& stream);
+    };
+
+    struct GlobalLayerMaskInfo
+    {
+        be<uint32_t> length;
+        be<uint16_t> overlay_colorspace;
+        be<uint16_t> color_component[4];
+        be<uint16_t> opacity; // 0 = transparent 100 = opaque
+        uint8_t kind;
+
+        bool read(std::istream& stream);
+        bool write(std::ostream& stream);
+    };
+
 #pragma pack(pop)
 
     class psd
     {
         public:
             psd();
-            psd(std::istream&& stream);
+            template <typename Stream>
+            psd(Stream&& stream)
+            {
+                load(stream);
+            }
 
-            bool load(std::istream&& stream);
-            bool save(std::ostream&& f);
+            bool load(std::istream& stream);
+            bool save(std::ostream& f);
 
             Header header;
+            std::vector<ImageResourceBlock> image_resources;
+            LayerInfo layer_info;
+            GlobalLayerMaskInfo global_layer_mask_info;
+            std::vector<Layer>& layers{layer_info.layers};
+            std::vector<ImageData>& channel_image_datas{layer_info.channel_image_datas};
 
             operator bool();
         private:
-            bool read_header(std::istream&& f);
-            bool read_color_mode(std::istream&& f);
-            bool read_image_resources(std::istream&& f);
+            bool read_header(std::istream& f);
+            bool read_color_mode(std::istream& f);
+            bool read_image_resources(std::istream& f);
+            bool read_layers_and_masks(std::istream& f);
 
-            bool write_header(std::ostream&& f);
-            bool write_color_mode(std::ostream&& f);
-            bool write_image_resources(std::ostream&& f);
+            bool read_layer_info(std::istream& f);
+
+            bool write_header(std::ostream& f);
+            bool write_color_mode(std::ostream& f);
+            bool write_image_resources(std::ostream& f);
 
             bool valid_{false};
 
-            std::vector<ImageResourceBlock> image_resources;
     };
 
 }
